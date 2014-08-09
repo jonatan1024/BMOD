71c949abba9adfb052c1cc86c62961d82db8a7e4
Jonatan Matějka <jonatan1024@gmail.com>
2014-08-09 20:25:03 +0200

    v140818 linux

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ g++ -m32 -static-libstdc++ -Wno-write-strings -Wno-attributes -D stricmp=strcase
 -I ../hlsdk/common/ -I ../hlsdk/dlls/ -I ../hlsdk/engine -I ../bullet/src/ -I ../metamod/ -I ../amxmodx/ \
 -L ../bullet/src/.libs/ \
 -Wl,--start-group \
-call.cpp forwards.cpp main.cpp model.cpp natives.cpp object.cpp sdk/amxxmodule.cpp \
+call.cpp forwards.cpp main.cpp model.cpp natives.cpp object.cpp sdk/amxxmodule.cpp euler.cpp debug.cpp \
 -Wl,--end-group \
 -l BulletCollision -l BulletDynamics -l LinearMath \
 -shared -o amxmodx_modules/bmod_amxx_i386.so
